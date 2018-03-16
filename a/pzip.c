3238eb98ad0c71f4c9645c996bfe3edca55e7841
jviall <viall@rockhopper-05.cs.wisc.edu>
2018-03-16 16:10:49 -0500

    mmap works. We've got a lil cat-like loop to test it

--- a/pzip.c
+++ a/pzip.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
 	if(argc < 2){
@@ -8,38 +11,37 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	
-	FILE * fp;
+	int fp;
 
-	int first = -1;
-	int curr;
-	size_t runLength = 0;
+	//int first = -1;
+	//int curr;
+	//size_t runLength = 0;
 
 
 	for(size_t i = 1; i < argc; i++){
-		fp = fopen(argv[i], "r");
-		if (fp==NULL){
+		if ((fp = open(argv[i], O_RDONLY)) < 0){
 			printf("my-zip: cannot open file\n");
 			exit(1);
+		}	
+		char * src;
+		struct stat statbuff;
+
+		if (fstat (fp, &statbuff) < 0){
+			printf("fstat error\n");
+			exit(1);
 		}
-		
-		while ( (curr = fgetc(fp)) != EOF ){
-			if(first == -1) first = curr;
-			
-			if( curr == first ) runLength++; 
-			else {
-				// end of run
-				//printf("%ld%c", runLength, (char)first);
-				fwrite(&runLength, 4, 1, stdout);
-				putc((char)first, stdout);
-				first = curr;
-				runLength = 1;
-			}
-		}
-		fclose(fp);
+		if ((src = mmap (0, statbuff.st_size, PROT_READ, MAP_SHARED, fp, 0)) == (caddr_t) -1){
+			printf ("mmap error for input");
+      return 0;
+   	}
+		///*nice kitty
+		for(int i = 0; i < statbuff.st_size; i++)
+			printf("%c", *(src + i));
+		//*/
 	}
 	//printf("%ld%c", runLength, (char)first);
-	fwrite(&runLength, 4, 1, stdout);
-	putc((char)first, stdout);
+	//fwrite(&runLength, 4, 1, stdout);
+	//putc((char)first, stdout);
 
-  	return 0;
+  return 0;
 }
