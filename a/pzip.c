685a5ca453d024659873f6a5cfca17d65556a31c
James Viall <viall@rockhopper-05.cs.wisc.edu>
2018-03-16 19:31:40 -0500

    Linked list segment structure works. But wierdly our character count of 3MB's of 0's counted 100x too many.

--- a/pzip.c
+++ a/pzip.c
@@ -1,9 +1,28 @@
+/*
+///////////////////////////////////////////////////////////////////////////////
+///////CCCC////////////////////////////////////////////////////////////////////
+/////C     C///////////////////////////////////////////////////////////////////
+////C//////////////////////////////////////////////////////////////////////////
+////C//////////////////////////////////////////////////////////////////////////
+////C//////////////////////////////////////////////////////////////////////////
+/////C/////C///////////////////////////////////////////////////////////////////
+///////CCCC////////////////////////////////////////////////////////////////////
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/sysinfo.h>
+
+struct segment {
+	char * ptr;
+	int len;	//at most chunkSize
+	int segNum;
+	struct segment *next;
+};
 
 int main(int argc, char *argv[]) {
 	// cmd syntax check
@@ -12,14 +31,18 @@ int main(int argc, char *argv[]) {
 		printf("my-zip: file1 [file2 ...]\n");
 		return 1;
 	}
+	// 1 thread per available processor
+	//int nthreads = get_nprocs();
 
 	int fp;
-	//char * dest;
 	char * srcs[argc-1];
 	struct stat statbuff[argc-1];
-	int first = -1;
-	char * curr;
+	int firstChar = -1;
+	char * curChar;
 	size_t runLength = 0;
+	size_t chunkSize = 0x100000; 	//1MB
+	int numChunks = 0;						// say "nun chucks"
+	struct segment * first = malloc(sizeof(struct segment));
 
 	//MMAP all our files to srcs[]
 	for(size_t i = 1; i < argc; i++){
@@ -42,21 +65,69 @@ int main(int argc, char *argv[]) {
 		close(fp);	
 	}
 
-	// zipperino
+	// Build linked list of segments
+	first->segNum = 1;
+	struct segment * curChunk = first;
 	for(size_t i = 1; i < argc; i++){
-		for(int a = 0; a < statbuff[i-1].st_size; a++){
-			//printf("%c", *(srcs[i-1] + a));
-			curr = (srcs[i-1]+a);
-			if(first == -1) first = *curr;
-			if(*curr == first) runLength++;
-			else {	// end of run
-				//printf("%ld%c", runLength, (char)first);
-				fwrite(&runLength, 4, 1, stdout);
-				putc((char)first, stdout);
-				first = *curr;
-				runLength = 1;
-			} 
+		int size = statbuff[i-1].st_size;
+		char * position = srcs[i-1];
+		//first segment
+		if(i == 1){
+			first->ptr = position;
+			numChunks++;
+			//base case: size of rest of file is less than chunk size
+			if(size < chunkSize){
+				first->len = size;
+				continue;
+			}
+			first->len = chunkSize;
+			position += chunkSize;
+			size -= chunkSize;
 		}
+		//while this file can still be segmented
+		while(size > 0){
+			numChunks++;
+			struct segment *next = malloc(sizeof(struct segment));
+			curChunk->next = next;
+			//base case: size of rest of file is less than chunk size -- go to next file
+			if(size < chunkSize){
+				next->segNum = curChunk->segNum+1;
+				next->len = size;
+				next->ptr = position;
+				curChunk = next;
+				break;
+			}
+			//
+			else{
+				next->segNum = curChunk->segNum+1;
+				next->len = chunkSize;
+				next->ptr = position;
+				position += chunkSize;
+				size -= chunkSize;
+				curChunk = next;	
+			}
+		}
+	}//end linked list building
+
+	curChunk = first;
+	while(curChunk){
+		printf("\nTotal Chunks = %d | numChunk: %d | chunk length: %d\n", numChunks, curChunk->segNum, curChunk->len);
+	  for(int i = 0; i < curChunk->len ; i++){
+			curChar = (curChunk->ptr+i);
+			if(firstChar == -1) firstChar = *curChar;
+			if(*curChar == firstChar) runLength++;
+			else {	// end of run
+				printf("%ld%c", runLength, (char)(firstChar + 1));
+				//fwrite(&runLength, 4, 1, stdout);
+				//putc((char)firstChar, stdout);
+				firstChar = *curChar;
+				runLength = 1;
+			}
+		}
+		struct segment *prev = curChunk;
+		curChunk = curChunk->next;
+		free(prev); 
 	}
+	printf("%ld-%c", runLength, (char)(firstChar+1));
 	return 0;
 }
